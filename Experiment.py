d13f996302bbc6d38c889bb015e08780ea0fb45f
Hongbo <lhb1242410833@163.com>
2023-01-01 21:42:10 -0500

    little modify

--- Experiment.py
+++ Experiment.py
@@ -11,9 +11,6 @@ _JSON_EXTS = [".json"]
 
 class Experiment:
     def __init__(self, exp_path) -> None:
-        self.exp_config = dict()
-        self.exp_module_list = list()
-
         if not os.path.exists(exp_path):
             raise FileNotFoundError(
                 errno.ENOENT, os.strerror(errno.ENOENT), exp_path)
@@ -22,18 +19,22 @@ class Experiment:
             raise FileNotFoundError("File is not {} format".format(_JSON_EXTS))
 
         with open(exp_path, 'r') as f:
-            self.exp_config = json.load(f)
+            exp_config = json.load(f)
 
-        self.init_exp_module_list()
+        self.init_exp_module_list(exp_config)
 
-    def init_exp_module_list(self):
-        self.exp_module_list.append(DataJsonParser(self.exp_config))
-        self.exp_module_list.append(ModelJsonParser(self.exp_config))
+    def init_exp_module_list(self, exp_config: dict):
+        self.data_config = DataJsonParser(exp_config)
+        self.model_config = ModelJsonParser(exp_config)
 
-    def save(self):
-        if len(self.exp_module_list) > 0:
-            result = dict()
-            result['exp_config'] = self.exp_config
-            for module in self.exp_module_list:
-                module.json_serialize(result)
-            print(result)
+    def init_rst_module_list(self):
+        self.timer_stats = None
+        self.dataset_stats = None
+        self.metric_stats = None
+
+    def save(self, result_path):
+        result = dict()
+        result['exp_config'] = self.exp_config
+        for module in self.exp_module_list:
+            module.json_serialize(result)
+        print(result)
