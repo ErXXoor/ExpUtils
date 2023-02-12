9112e39326bf30e15d6b5c968f2319add56c6d13
Hongbo <hm9026@wayne.edu>
2023-02-12 16:53:57 -0500

    simplified

--- Experiment.py
+++ Experiment.py
@@ -15,6 +15,7 @@ class Experiment:
         
         self._data_config = exp_config.data_config
         self._model_config = exp_config.model_config
+        self._utils_config = exp_config.utils_config
         
         self.validate_path(exp_path)
         
@@ -29,11 +30,11 @@ class Experiment:
         if not os.path.exists(os.path.dirname(result_path)):
             os.makedirs(os.path.dirname(result_path))
             
-            self._exp_result.update(self._timer_stats.serialize())
-            self._exp_result.update(self._model_config)
-            self._exp_result.update(self._data_config)
+        self._exp_result.update(self._timer_stats.serialize())
+        self._exp_result.update(self._model_config)
+        self._exp_result.update(self._data_config)
             
-            cfg_to_json(self._exp_result, result_path)
+        cfg_to_json(self._exp_result, result_path)
 
     def validate_path(self, file_path):
         if Path(file_path).suffix not in _JSON_EXTS:
